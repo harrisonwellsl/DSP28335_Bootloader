9a8f9d9a44cd51bb1adefdd19123097534ae67d6
harrisonwellsl <harrisonwellsl@outlook.com>
2020-10-28 21:08:00 +0800

    完成SCI初始化函数

--- src/user/sci_driver.c
+++ src/user/sci_driver.c
@@ -56,5 +56,41 @@ void sci_init(enum SCI_DEVICE sci_device, SCI_ATTR* sci_attr) {
             ((LSPCLK_SYS / (sci_dev[sci_device]->boundrate * 8) - 1) >> 8) & 0xFF;
     sci_dev[sci_device]->sci_regs->SCIHBAUD =
             (LSPCLK_SYS / (sci_dev[sci_device]->boundrate * 8) - 1) & 0xFF;
+
+    /* 让发送FIFO处于复位状态，等待配置完成后释放 */
+    sci_dev[sci_device]->sci_regs->SCIFFTX.bit.TXFIFOXRESET = 0;
+
+    /* 复位发送FIFO */
+    sci_dev[sci_device]->sci_regs->SCIFFTX.bit.SCIRST = 0;
+
+    /* 清除SCI接收中断标志位 */
+    sci_dev[sci_device]->sci_regs->SCIFFTX.bit.TXFFINTCLR = 1;
+
+    /* 让接收FIFO处于复位状态，等待配置完成后释放 */
+    sci_dev[sci_device]->sci_regs->SCIFFRX.bit.RXFIFORESET = 0;
+
+    /* 清除接收FIFO的过载位 */
+    sci_dev[sci_device]->sci_regs->SCIFFRX.bit.RXFFOVRCLR = 1;
+
+    /* 清除接收FIFO的中断位 */
+    sci_dev[sci_device]->sci_regs->SCIFFRX.bit.RXFFINTCLR = 1;
+
+    /* 不使能FIFO中断 */
+    sci_dev[sci_device]->sci_regs->SCIFFRX.bit.RXFFIENA = 0;
+
+    /* 禁止波特率自动协商 */
+    sci_dev[sci_device]->sci_regs->SCIFFCT.bit.CDC = 0;
+
+    /* 将发送FIFO从复位状态中使能出来 */
+    sci_dev[sci_device]->sci_regs->SCIFFTX.bit.TXFIFOXRESET = 1;
+
+    /* 将接收FIFO从复位状态中使能出来 */
+    sci_dev[sci_device]->sci_regs->SCIFFRX.bit.RXFIFORESET = 1;
+
+    /* 使能发送FIFO */
+    sci_dev[sci_device]->sci_regs->SCIFFTX.bit.SCIFFENA = 1;
+
+    /* 使能中断 */
+    sci_dev[sci_device]->sci_regs->SCICTL2.bit.RXBKINTENA = 1;
     EDIS;
 }
